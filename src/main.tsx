1f9d511a9e3114ab2401e2b259b360c5fa7b7def
jesica <112604050+samuelsitio26@users.noreply.github.com>
2026-03-03 15:45:34 +0700

    Initial commit: FilterPage assessment

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import FilterPage, { loader } from './FilterPage';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <FilterPage />,
+    loader: loader,
+  },
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
